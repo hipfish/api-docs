83ba339c82b15f0d8824b53cd4262f0e04969d2f
Chris Gough <chris.gough@environment.gov.au>
2016-12-28 14:12:34 +1100

    remove API word from conf.py

--- conf.py
+++ conf.py
@@ -3,7 +3,7 @@ extensions = ['sphinx.ext.graphviz']
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
-project = u'HiPFiSH API docs'
+project = u'HiPFiSH docs'
 copyright = u'2016, Chris Gough'
 version = '0.0'
 release = '0.0'
@@ -11,19 +11,19 @@ exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 html_theme = 'default'
 html_static_path = ['_static']
-htmlhelp_basename = 'HiPFiSHAPIdocsdoc'
+htmlhelp_basename = 'HiPFiSHdocs'
 latex_elements = {
 }
 latex_documents = [
-  ('index', 'HiPFiSHAPIdocs.tex', u'HiPFiSH API docs Documentation',
+  ('index', 'HiPFiSHdocs.tex', u'HiPFiSH Documentation',
    u'Chris Gough', 'manual'),
 ]
 man_pages = [
-    ('index', 'hipfishapidocs', u'HiPFiSH API docs Documentation',
+    ('index', 'hipfishdocs', u'HiPFiSH Documentation',
      [u'Chris Gough'], 1)
 ]
 texinfo_documents = [
-  ('index', 'HiPFiSHAPIdocs', u'HiPFiSH API docs Documentation',
-   u'Chris Gough', 'HiPFiSHAPIdocs', 'One line description of project.',
+  ('index', 'HiPFiSHAPIdocs', u'HiPFiSH Documentation',
+   u'Chris Gough', 'HiPFiSHdocs', 'One line description of project.',
    'Miscellaneous'),
 ]
